2f7f24d0f7d7909e64a1e72552a9f4bb2f57a075
Trey Hakanson <treyhakanson@gmail.com>
2018-05-13 00:12:11 -0400

    🔧 generalize query execution

--- src/player-efficacy.py
+++ src/player-efficacy.py
@@ -6,11 +6,36 @@ from constants import (TEAM_NAMES, DB_PATH, RUSH_TYPES, ROUTE_TYPES)
 from utils import avg_cols
 
 PICKLE_DIR = "../efficacies/pickles"  # directory to store pickled dictionaries
-REPICKLE = False                      # override existing pickles
+REPICKLE = True                      # override existing pickles
 
 conn = sqlite3.connect(DB_PATH)
 c = conn.cursor()
 pp = PrettyPrinter(width=100)
+
+
+def execute_def_query(cursor, table_name, col_out, col_in):
+    def_avgs = {"league_avg": []}
+    for team in TEAM_NAMES:
+        query_args = ["%%%s%%" % team.replace(" ", "_"), team]
+        c.execute("""
+            SELECT %s
+                FROM (
+                    SELECT %s
+                    FROM %s t
+                        JOIN snap_count sc ON
+                            t.player_link = sc.player_link AND
+                            t.game = sc.game
+                    GROUP BY t.game, sc.team_name
+                    HAVING t.game LIKE ? AND
+                           sc.team_name NOT LIKE ?
+                )
+        """ % (col_out, col_in, table_name), query_args)
+        res = list(c.fetchone())
+        def_avgs[team] = res
+        def_avgs["league_avg"].append(res)
+    def_avgs["league_avg"] = avg_cols(def_avgs["league_avg"])
+    return def_avgs
+
 
 # build rush defense averages (average yards yielded per attempt, by type)
 print("Analyzing rush defenses...")
@@ -24,25 +49,7 @@ else:
     col_in_tmpl = "SUM(%s__yds) / SUM(%s__att) %s__per_att"
     col_out = ",".join([col_out_tmpl % (typ, typ) for typ in RUSH_TYPES])
     col_in = ",".join([col_in_tmpl % (typ, typ, typ) for typ in RUSH_TYPES])
-    rush_def_avgs = {"league_avg": []}
-    for team in TEAM_NAMES:
-        c.execute("""
-            SELECT %s
-                FROM (
-                    SELECT %s
-                    FROM rush_dir rd
-                        JOIN snap_count sc ON
-                            rd.player_link = sc.player_link AND
-                            rd.game = sc.game
-                    GROUP BY rd.game, sc.team_name
-                    HAVING rd.game LIKE ? AND
-                           sc.team_name NOT LIKE ?
-                )
-        """ % (col_out, col_in), ["%%%s%%" % team.replace(" ", "_"), team])
-        res = list(c.fetchone())
-        rush_def_avgs[team] = res
-        rush_def_avgs["league_avg"].append(res)
-    rush_def_avgs["league_avg"] = avg_cols(rush_def_avgs["league_avg"])
+    rush_def_avgs = execute_def_query(c, "rush_dir", col_out, col_in)
     with open(rush_def_avgs_fname, "wb") as file:
         pickle.dump(rush_def_avgs, file)
 
@@ -66,25 +73,7 @@ else:
         col_in_arr.append(col_in_tmpl2 % (typ, typ, typ))
     col_out = ",".join(col_out_arr)
     col_in = ",".join(col_in_arr)
-    pass_def_avgs = {"league_avg": []}
-    for team in TEAM_NAMES:
-        c.execute("""
-            SELECT %s
-                FROM (
-                    SELECT %s
-                    FROM tgt_dir td
-                        JOIN snap_count sc ON
-                            td.player_link = sc.player_link AND
-                            td.game = sc.game
-                    GROUP BY td.game, sc.team_name
-                    HAVING td.game LIKE ? AND
-                           sc.team_name NOT LIKE ?
-                )
-        """ % (col_out, col_in), ["%%%s%%" % team.replace(" ", "_"), team])
-        res = list(c.fetchone())
-        pass_def_avgs[team] = res
-        pass_def_avgs["league_avg"].append(res)
-    pass_def_avgs["league_avg"] = avg_cols(pass_def_avgs["league_avg"])
+    pass_def_avgs = execute_def_query(c, "tgt_dir", col_out, col_in)
     with open(pass_def_avgs_fname, "wb") as file:
         pickle.dump(pass_def_avgs, file)
 
